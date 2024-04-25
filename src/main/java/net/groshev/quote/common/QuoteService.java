8f9b0e809f34f123320e3d2f84a0904b2042324a
Konstantin Groshev <real.mt@gmail.com>
2024-04-25 13:26:20 +0300

    [+] switch syntaxis

--- src/main/java/net/groshev/quote/common/QuoteService.java
+++ src/main/java/net/groshev/quote/common/QuoteService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 @Component
 public class QuoteService {
     @Value("${QUOTE_SERVICE:backToFuture}")
